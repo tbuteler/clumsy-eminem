ac817cd9b48dec96eb55bbd851d0edb159637d3f
Tomas Buteler <tomas.buteler@wiz.pt>
2014-08-25 12:53:46 +0100

    Modified bootstrap asset key so it plays nice with clumsy/cms

--- src/assets/assets.php
+++ src/assets/assets.php
@@ -7,7 +7,7 @@ return array(
 		'path'	=> '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
 	),
 
-    'bootstrap.css' => array(
+    'bootstrap' => array(
         'set'   => 'styles',
         'path'  => '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css',
     ),
@@ -21,7 +21,7 @@ return array(
     'media-management.css' => array(
         'set'   => 'styles',
         'path'  => 'packages/clumsy/eminem/css/media-management.css',
-        'req'   => 'bootstrap.css',
+        'req'   => 'bootstrap',
         'v'     => '0.1',
     ),
     
