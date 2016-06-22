4e80624ecdfa94c9fb0e2825392a32d29625e683
Tomas Buteler <tomas.buteler@wiz.pt>
2016-06-22 19:33:33 +0100

    Pass position to placeholder pivot data

--- src/Traits/Mediable.php
+++ src/Traits/Mediable.php
@@ -60,7 +60,7 @@ trait Mediable
     {
         $media = MediaManager::media();
         $media->path = $this->mediaPlaceholder($position);
-        $media->setRelation('pivot', $this->media()->newPivot());
+        $media->setRelation('pivot', $this->media()->newPivot(compact('position')));
         return $media;
     }
 
