dde5e540e22d647ac3452f17b86434f616775801
Tomas Buteler <tomas.buteler@wiz.pt>
2017-02-11 11:07:28 +0000

    Compatibility with new Clumsy Assets API

--- src/assets/assets.php
+++ src/assets/assets.php
@@ -3,28 +3,30 @@
 return [
 
     'jquery' => [
-        'set'   => 'footer',
-        'path'  => '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+        'set'     => 'footer',
+        'path'    => '//ajax.googleapis.com/ajax/libs/jquery/{{version}}/jquery.min.js',
+        'version' => '3.1.1',
     ],
 
     'bootstrap.js' => [
-        'set'   => 'footer',
-        'path'  => '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
-        'req'   => 'jquery',
+        'set'      => 'footer',
+        'path'     => '//maxcdn.bootstrapcdn.com/bootstrap/{{version}}/js/bootstrap.min.js',
+        'version'  => '3.3.7',
+        'requires' => 'jquery',
     ],
 
     'media-management.css' => [
-        'set'    => 'styles',
-        'path'   => 'vendor/clumsy/eminem/css/media-management.css',
-        'v'      => '0.14.0',
-        'elixir' => false,
+        'set'     => 'styles',
+        'path'    => 'vendor/clumsy/eminem/css/media-management.css',
+        'hash'    => false,
+        'version' => '0.14.0',
     ],
 
     'media-management.js' => [
-        'set'    => 'footer',
-        'path'   => 'vendor/clumsy/eminem/js/media-management.min.js',
-        'req'    => 'bootstrap.js',
-        'v'      => '0.14.0',
-        'elixir' => false,
+        'set'      => 'footer',
+        'path'     => 'vendor/clumsy/eminem/js/media-management.min.js',
+        'requires' => 'bootstrap.js',
+        'hash'     => false,
+        'version'  => '0.14.0',
     ],
 ];
